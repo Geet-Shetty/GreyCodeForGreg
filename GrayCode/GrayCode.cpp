fdb33abf5d754a75c893d5907953a76d74e377cf
Geet Shetty <gshetty902@gmail.com>
2026-06-27 21:48:39 -0700

    clean up code and add verification function

--- GrayCode/GrayCode.cpp
+++ GrayCode/GrayCode.cpp
@@ -22,13 +22,19 @@ struct GreyCodeSequence
 /// </summary>
 /// <param name="n">number of bits</param>
 /// <returns>Grey Code Sequence</returns>
-static GreyCodeSequence GreyCode(uint8_t n)
+GreyCodeSequence GreyCode(uint8_t n)
 {
-    // add logic to have range for n? 
-    // switch to using shifts for pow ops
+    // can't go above 64 bits with uint64_t
+    if (n < 65)
+    {
+        return {}; 
+    }
     
     // set up information for return value
     GreyCodeSequence gcSeq{}; 
+
+    // used pow instead of shift because for n = 64 would need 65 bits 
+    // could use shift until 64 but honestly timed saved doesn't matter
     gcSeq.sequence_len = (uint64_t)pow(2,n);
     gcSeq.nbits = n; 
 
@@ -37,12 +43,14 @@ static GreyCodeSequence GreyCode(uint8_t n)
     prev_seq[1] = 1; 
     uint64_t prev_seq_len = 2; 
 
+    // each sequence is built from the previous one
     for (uint64_t i = 1; i < (uint64_t)n; i++)
     {
         // create next sequence list 
         uint64_t seq_len = (uint64_t)pow(2, i + 1);
         uint64_t* seq = new uint64_t[seq_len](); 
 
+        // create MSB zero section 
         uint64_t j = 0; 
         for (;j < prev_seq_len && seq_len == (2 * prev_seq_len); j++)
         {
@@ -53,6 +61,7 @@ static GreyCodeSequence GreyCode(uint8_t n)
         // the mid point 
         uint64_t index = j; 
 
+        // create MSB one section 
         uint64_t k = prev_seq_len - 1; 
         do
         {
@@ -66,27 +75,66 @@ static GreyCodeSequence GreyCode(uint8_t n)
             k--; 
         } 
         while (k > 0);
+
+        // do while exits before last element 
         if (index == seq_len - 1)
         {
             seq[index] = prev_seq[k] | (1i64 << i);
         }
 
+        // update previous sequence 
         prev_seq = seq; 
         prev_seq_len = seq_len; 
     }
 
+    // set return value 
     gcSeq.sequence = prev_seq; 
 
     return gcSeq;
 }
 
+/// <summary>
+/// Verifies GreyCode Sequence, checks that sequence follow 1 bit change rule
+/// </summary>
+/// <param name="greyCode">Grey Code Sequence to check</param>
+/// <returns>true for valid grey code and false for invalid greycode</returns>
+bool VerifyGreyCode(GreyCodeSequence greyCode)
+{
+    for (uint64_t i = 1; i < greyCode.sequence_len; i++)
+    {
+        int bitChanges = 0; 
+
+        for (uint64_t j = 0; j < greyCode.nbits; j++)
+        {
+            uint64_t curr_masked = greyCode.sequence[i] & (1i64 << j); 
+            uint64_t prev_masked = greyCode.sequence[i - 1] & (1i64 << j);
+
+            if (curr_masked ^ prev_masked)
+            {
+                bitChanges++; 
+            }
+        }
+
+        if (bitChanges > 1)
+        {
+            std::cout << "Bitchanges: " << bitChanges << std::endl;
+            return false; 
+        }
+    }
+}
+
 int main()
 {
-    GreyCodeSequence greyCode = GreyCode(4); 
+    GreyCodeSequence greyCode = GreyCode(5); 
 
     for (uint64_t i = 0; i < greyCode.sequence_len; i++)
     {
         std::cout << greyCode.sequence[i] << std::endl; 
+    }
+
+    if (!VerifyGreyCode(greyCode))
+    {
+        std::cout << "WORKED" << std::endl;
     }
 
     int x; 
